df73cadfa338452d25c185c941a9f0d667208ec5
mormic21 <mormichi4@gmail.com>
2021-04-14 20:12:58 +0200

    Kommentare bei drehmatrix

--- drehmatrix/drehmatrix.c
+++ drehmatrix/drehmatrix.c
@@ -1,3 +1,10 @@
+/*
+ * drehmatrix.c. Mithilfe der Drehmatrix wird ein Punkt einmal im Kreis gedreht.
+ * Dabei werden 1-Grad-Schritte gemacht. Die Ausgabe kann in eine csv-datei ausgegeben werden.
+ * 14.04.2021
+ * @author: Michael Morandell
+ */
+
 #include <stdio.h>
 #include "matrix.h"
 #include <math.h>
@@ -10,8 +17,8 @@ int main(void) {
 	m1.spalten = 2;
 	p.zeilen = 2;
 	p.spalten = 1;
-	//
-	double winkel = 1*(M_PI/180);
+	//Ein Grad-Winkel
+	double winkel = M_PI/180;
 	m1.matrix[0][0] = cos(winkel);
 	m1.matrix[0][1] = -sin(winkel);
 	m1.matrix[1][0] = sin(winkel);
@@ -20,7 +27,9 @@ int main(void) {
 	p.matrix[0][0] = 1;
 	//y-koordinate
 	p.matrix[1][0] = 0;
+	//Es wird 360 Mal eine ein-Grad-Drehung vollzogen.
 	for (int i = 0; i < 360; i++) {
+		//Ausgabe (siehe module.c)
 		printmatrix(p);
 		p = matrixmult(m1, p);
 	}
